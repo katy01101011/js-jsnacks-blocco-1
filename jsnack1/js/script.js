eee08440059b6f088aead54ee74748ad7658f86c
katy01101011 <katy.91560@gmail.com>
2022-04-19 15:38:19 +0200

    add: 1snack

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,1 +1,13 @@
-alert('Ciao')
+// L'utente inserisce due parole in successione, con due prompt.
+const firstWord = prompt('Inserisci una parola')
+console.log(firstWord);
+
+const secondWord = prompt('Inserisci un\'altra parola')
+console.log(secondWord);
+
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+if (firstWord.length < secondWord.length) {
+    console.log(firstWord, secondWord);
+} else if (firstWord.length > secondWord.length) {
+    console.log(secondWord, firstWord);
+}

21b245d723bec721492a892414724061f32deb4c
katy01101011 <katy.91560@gmail.com>
2022-04-19 16:32:08 +0200

    add: "jsnack4"

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,1 +1,20 @@
-alert('Ciao')
+// Chiedi un numero di 4 cifre all'utente
+const userNumber = prompt('Inserisci un numero a 4 cifre');
+console.log(userNumber);
+
+// e calcola la somma di tutte le cifre che compongono il numero.
+const numberArray = [];
+let stringNumber = '';
+let somma = 0;
+
+for (let i = 0; i < 4; i++) {
+    stringNumber = parseInt(userNumber.charAt(i));
+    console.log(stringNumber);
+    
+    numberArray.push(stringNumber)
+
+    somma += stringNumber;
+}
+
+console.log(numberArray);
+console.log(somma);

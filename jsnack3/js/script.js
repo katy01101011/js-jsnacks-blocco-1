67372cd68c05de23e9eb6d4a79d6cd7271835824
katy01101011 <katy.91560@gmail.com>
2022-04-19 15:57:15 +0200

    add: "jsnack3"

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,15 @@
-alert('Ciao')
+// Calcola la somma e la media dei primi 10 numeri.
+tenNumbers = [];
+
+let somma = 0;
+
+for (let i = 1; i <=10; i++) {
+    console.log(i);
+
+    somma += i;
+}
+
+console.log(somma);
+
+const media = (somma / 10);
+console.log(media);

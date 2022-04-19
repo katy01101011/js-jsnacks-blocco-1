1089f14861e4478c60fc67016219953fbf12afa2
katy01101011 <katy.91560@gmail.com>
2022-04-19 15:45:55 +0200

    add: "jsnack2"

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,1 +1,17 @@
-alert('Ciao')
+// Crea un array vuoto.
+const oddNumbers = [];
+
+// Chiedi per 6 volte all'utente di inserire un numero,
+let userNumber = '';
+
+for (let i = 0; i < 6; i++) {
+    userNumber = parseInt(prompt('Inserisci un numero'));
+    console.log(userNumber);
+
+    if (userNumber % 2 === 1) {
+        oddNumbers.push(userNumber)
+    }
+}
+
+// se è dispari inseriscilo nell'array.
+console.log(oddNumbers);
